29bf046e6dffddb19cbba6da676449a2d65892fc
Kanilionis <kanilionis@gmail.com>
2020-10-23 12:13:53 -0600

    added gitignore file and removed nodemodules from repo

--- Develop/db/notes.js
+++ Develop/db/notes.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 const util = require("util")
-var id = 0
+var id = 1
 const readFileAsync = util.promisify(fs.readFile)
 const writeFileAsync = util.promisify(fs.writeFile)
 
